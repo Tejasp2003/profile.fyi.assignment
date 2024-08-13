46fe8c0b519f727c74866ef523ba8a9e16db66e7
Tejasp2003 <tejasp200320@gmail.com>
2024-08-13 11:42:22 +0530

    added toasts and checkout page (dummy)

--- client/src/components/Checkout.tsx
+++ client/src/components/Checkout.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-const Checkout = () => {4
+const Checkout = () => {
   const navigate = useNavigate();
 
   useEffect(
@@ -17,8 +17,7 @@ const Checkout = () => {4
   );
   return (
     <div className="flex justify-center items-center min-h-screen bg-green-200">
-      {/*You have paced your order
-       */}
+     
       <span className="grainy text-green-400 font-semibold text-xl p-4 rounded-xl ">
         You have placed your order successfully 🎉
       </span>
